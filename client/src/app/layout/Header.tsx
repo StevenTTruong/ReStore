4765eecc4db0632bb7129b73588adab8ad613ac2
Steven Truong <s.ttruong@yahoo.com>
2024-04-15 16:32:48 -0400

    catalog view routing

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -35,7 +35,7 @@ export default function Header({ darkMode, handleThemeChange }: Props) {
         <AppBar position='static' sx={{ mb: 4 }}>
             <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
 
-                <Box>
+                <Box display = 'flex' alignItems='center'>
 
                     <Typography variant='h6' component={NavLink}
                         to='/'
@@ -61,7 +61,7 @@ export default function Header({ darkMode, handleThemeChange }: Props) {
                     ))}
                 </List>
 
-                <Box>
+                <Box display = 'flex' alignItems='center'>
                     <IconButton size='large' edge='start' color='inherit' sx={{ mr: 2 }}>
                         <Badge badgeContent='4' color='secondary'>
                             <ShoppingCart />
