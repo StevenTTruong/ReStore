5ee2770217884d1c7993ab00fc413d316937e453
Steven Truong <s.ttruong@yahoo.com>
2024-04-25 09:54:31 -0400

    added shopping cart

--- API/Entities/Basket.cs
+++ API/Entities/Basket.cs
@@ -2,11 +2,11 @@ namespace API.Entities
 {
     public class Basket
     {
-        public int Id {get; set;}
+        public int Id { get; set ;}
 
-        public string BuyerId {get; set;}
+        public string BuyerId { get; set ;}
 
-        public List<BasketItem> Items {get; set;} = new();
+        public List<BasketItem> Items { get; set ;} = new();
 
         public void AddItem(Product product, int quantity){
             if (Items.All(item => item.ProductId != product.Id)){
